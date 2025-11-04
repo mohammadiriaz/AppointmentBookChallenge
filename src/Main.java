257ef22a3ce15805fc184f2ea2a7d35c930d1cff
mohammadiriaz <mohammadr95@nycstudents.net>
2025-11-03 19:06:47 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,54 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String[] args){
 
-boolean[][] schedule = new boolean [8][60];
-AppointmentBook a = new AppointmentBook(schedule);
-for(int i = 10; i < 15; i++) schedule[1][i] = true;
-a.printPeriod(2);
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        boolean[][] schedule = new boolean[8][60];
+        AppointmentBook b = new AppointmentBook(schedule);
+
+        for (int i = 25; i < 30; i++) schedule[1][i] = true;
+        for (int i = 0; i < 15; i++) schedule[2][i] = true;
+        for (int i = 41; i < 60; i++) schedule[2][i] = true;
+        for (int i = 5; i < 30; i++) schedule[3][i] = true;
+        for (int i = 44; i < 60; i++) schedule[3][i] = true;
+
+        System.out.println(reads());
+    }
+
+    public static boolean[][] readSchedule(String lines) {
+        Scanner s = new Scanner(lines);
+        boolean[][] schedule = new boolean[8][60];
+
+        for (int i = 0; i < schedule.length; i++) {
+            for (int j = 0; j < schedule[i].length; j++) {
+                schedule[i][j] = s.nextBoolean();
+            }
+        }
+
+        return schedule;
+    }
+
+    public static int reads() throws FileNotFoundException{
+        int meetings = 0;
+        File f = new File("Schedules.txt");
+        Scanner s = new Scanner(f);
+        String schedule = "";
+
+        while (s.hasNext()) {
+            while (s.hasNextBoolean()) {
+                schedule += s.nextBoolean() + " ";
+            }
+
+            AppointmentBook a = new AppointmentBook(readSchedule(schedule));
+            schedule = "";
+
+            if (a.makeAppointment(s.nextInt(), s.nextInt(), s.nextInt())){
+                meetings++;
+            }
+        }
+        return meetings;
     }
 }
-
-
-
-
-
